9fbe5f119ee4d839324320be86acba21263e867a
PRIYANKA-BODA <priyankaboda550@gmail.com>
2025-09-07 11:29:13 +0530

    Add files via upload

---
+++ train_model.py
@@ -0,0 +1,40 @@
+import os
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D,MaxPooling2D,Flatten,Dense,Dropout
+IMZ_SIZE = 128
+BATCH_SIZE = 32
+EPOCHS =10
+#data Preparation
+datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
+#training the dataset
+train_data = datagen.flow_from_directory(
+    'garbage-dataset',
+    target_size=(IMZ_SIZE,IMZ_SIZE),
+    batch_size=BATCH_SIZE,
+    class_mode='categorical',
+    subset='training'
+)
+#validation data
+val_data = datagen.flow_from_directory(
+    "garbage-dataset",
+    target_size=(IMZ_SIZE,IMZ_SIZE),
+    batch_size=BATCH_SIZE,
+    class_mode='categorical',
+    subset='validation'
+)
+#CNN Model
+model = Sequential([
+    Conv2D(32, (3,3), activation='relu', input_shape= (IMZ_SIZE,IMZ_SIZE,3)),
+    MaxPooling2D(2,2),
+    Conv2D(64, (3,3), activation='relu'),
+    MaxPooling2D(2,2),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dropout(0.5),
+    Dense(10, activation='softmax') 
+])
+model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
+model.fit(train_data, validation_data=val_data,epochs=EPOCHS)
+model.save("garbage_classifier_model.h5")
